1754b490a17f1d79629e37664ccb0d132d70aec3
tuantv-0547 <49260360+tuantv-0547@users.noreply.github.com>
2020-08-13 18:11:52 +0700

    Test CI/CD (#99)
    * Test CI/CD
    
    * Update bundle audit
    
    Co-authored-by: ttuan <tuantv.nhnd@gmail.com>

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
-  belongs_to :buyer, foreign_key: :buyer_id, class_name: User.name
-  belongs_to :seller, foreign_key: :seller_id, class_name: User.name
+  belongs_to :buyer, class_name: User.name
+  belongs_to :seller, class_name: User.name
   belongs_to :product_size
 end
